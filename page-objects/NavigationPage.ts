c5f47bbf157fcd5c0bebf967db62526bd0328bf2
Anastasiia Taushkanova <168531065+anastasiia-test@users.noreply.github.com>
2024-12-16 22:32:11 +0700

    test: add functions to Navigation page object

--- page-objects/NavigationPage.ts
+++ page-objects/NavigationPage.ts
@@ -11,4 +11,27 @@ export class NavigationPage {
         await this.page.getByText('Forms').click()
         await this.page.getByText('Form Layouts').click()
     }
+
+    async datePickerPage() {
+        await this.page.getByText('Forms').click()
+        await this.page.getByText('Datepicker').click()
+
+    }
+
+    async smartTablePage() {
+        await this.page.getByText('Table & Data').click()
+        await this.page.getByText('Smart Table').click()
+
+
+    }
+
+    async tooltipPage() {
+        await this.page.getByText('Modal & Overlays').click()
+        await this.page.getByText('Tooltip').click()
+    }
+
+    async toastrPage() {
+        await this.page.getByText('Modal & Overlay ').click()
+        await this.page.getByText('Toastr').click()
+    }
 }
--- tests/Navigation.spec.ts
+++ tests/Navigation.spec.ts
@@ -1,12 +1,11 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { NavigationPage } from '../page-objects/NavigationPage'
 
-test.beforeEach(async({page}) => {
-    await page.goto('http://localhost:4200', {timeout: 1000, waitUntil: "domcontentloaded"})
-
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/', { waitUntil: "networkidle" })
 })
 
-test('navigate to form page', async ({page}) =>{
+test('navigate to form page', async ({ page }) => {
     const navigateTo = new NavigationPage(page)
     await navigateTo.formLayoutsPage()
 })
