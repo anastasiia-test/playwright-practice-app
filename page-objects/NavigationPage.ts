ef4c868afd2d167474752534bac28fbc5c8e56dc
Anastasiia Taushkanova <168531065+anastasiia-test@users.noreply.github.com>
2024-12-17 21:56:28 +0700

    test: refactor methods

--- page-objects/NavigationPage.ts
+++ page-objects/NavigationPage.ts
@@ -8,30 +8,35 @@ export class NavigationPage {
     }
 
     async formLayoutsPage() {
-        await this.page.getByText('Forms').click()
+        await this.selectGroupMenu('Forms')
         await this.page.getByText('Form Layouts').click()
     }
 
     async datePickerPage() {
-        await this.page.getByText('Forms').click()
+        await this.selectGroupMenu('Forms')
         await this.page.getByText('Datepicker').click()
-
     }
 
     async smartTablePage() {
-        await this.page.getByText('Table & Data').click()
+        await this.selectGroupMenu('Tables & Data')
         await this.page.getByText('Smart Table').click()
-
-
     }
 
     async tooltipPage() {
-        await this.page.getByText('Modal & Overlays').click()
+        await this.selectGroupMenu('Modal & Overlays')
         await this.page.getByText('Tooltip').click()
     }
 
     async toastrPage() {
-        await this.page.getByText('Modal & Overlay ').click()
+        await this.selectGroupMenu('Modal & Overlays')
         await this.page.getByText('Toastr').click()
+    }
+    
+    private async selectGroupMenu(groupItemTitle: string) {
+        const groupMenuItem = this.page.getByTitle(groupItemTitle)
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded')
+        if (expandedState == "false") {
+            await groupMenuItem.click()
+        }
     }
 }
--- tests/Navigation.spec.ts
+++ tests/Navigation.spec.ts
@@ -5,7 +5,11 @@ test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/', { waitUntil: "networkidle" })
 })
 
-test('navigate to form page', async ({ page }) => {
+test('navigate to a page', async ({ page }) => {
     const navigateTo = new NavigationPage(page)
     await navigateTo.formLayoutsPage()
+    await navigateTo.datePickerPage()
+    await navigateTo.smartTablePage()
+    await navigateTo.toastrPage()
+    await navigateTo.tooltipPage()
 })
